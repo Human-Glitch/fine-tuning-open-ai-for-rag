6695f199f1e945ccfd356708d48587da68439f72
Kody Buss <kody.buss@zelis.com>
2024-11-12 22:28:17 -0600

    Updated comments.

--- fine-tuning-open-ai-for-rag/fine_tuning_open_ai_for_rag/main.py
+++ fine-tuning-open-ai-for-rag/fine_tuning_open_ai_for_rag/main.py
@@ -2,12 +2,10 @@
 import os
 import json
 import pandas as pd
+
 from openai import OpenAI
-
 from tqdm import tqdm
-import numpy as np
 from dotenv import load_dotenv
-
 from preprocessor import Preprocessor
 from open_ai_service import OpenAiService
 from open_ai_finetuner import OpenAIFineTuner
@@ -22,20 +20,25 @@ load_dotenv()  # take environment variables from .env.
 open_api_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 open_ai_service = OpenAiService(open_api_client)
 
+# Load and split the data
 train_df = Preprocessor.json_to_dataframe_with_titles(json.load(open('local_cache/train.json')))
 val_df = Preprocessor.json_to_dataframe_with_titles(json.load(open('local_cache/dev.json')))
 df = Preprocessor.get_diverse_sample(val_df, sample_size=100, random_state=42)
 
-# # Use progress_apply with tqdm for progress bar
+# Establish a baseline with the a diverse sample
 df["generated_answer"] = df.progress_apply(open_ai_service.answer_question, axis=1)
+
+# Save checkpoint
 df.to_json("local_cache/100_val.json", orient="records", lines=True)
 df = pd.read_json("local_cache/100_val.json", orient="records", lines=True)
 
 train_sample = Preprocessor.get_diverse_sample(train_df, sample_size=100, random_state=42)
 
+# Create fine-tuning prompts from data frame
 with open("local_cache/100_train.jsonl", "w") as f:
     f.write(Preprocessor.dataframe_to_jsonl(train_sample))
-    
+   
+# Fine-tune the model 
 fine_tuner = OpenAIFineTuner(
     training_file_path="local_cache/100_train.jsonl",
     model_name="gpt-4o-mini-2024-07-18",
@@ -47,6 +50,7 @@ model_id = fine_tuner.fine_tune_model()
 completion = open_ai_service.create(model_id)
 print(completion.choices[0].message)
 
+# Evaluate the answers from the fine-tuned model 
 df["ft_generated_answer"] = df.progress_apply(open_ai_service.answer_question, model=model_id, axis=1)
 
 # Compare the results by merging into one dataframe
@@ -54,7 +58,9 @@ evaluator = Evaluator(df)
 evaluator.evaluate_model(answers_column="ft_generated_answer")
 evaluator.plot_model_comparison(["generated_answer", "ft_generated_answer"], scenario="answer_expected", nice_names=["Baseline", "Fine-Tuned"])
 
+# Create checkpoint
 df.to_json("local_cache/100_val_ft.json", orient="records", lines=True)
 df = pd.read_json("local_cache/100_val_ft.json", orient="records", lines=True)
 
+# Plot the results comparing the two models
 evaluator.plot_model_comparison(["generated_answer", "ft_generated_answer"], scenario="idk_expected", nice_names=["Baseline", "Fine-Tuned"])
